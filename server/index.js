395a10f20c39c6926cc0803734bf5cac48878f1c
Srinath <srinath17121990@gmail.com>
2023-06-23 22:47:04 +0530

    Insert

--- server/index.js
+++ server/index.js
@@ -2,3 +2,22 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+import router from "./routes/posts.js";
+
+const app = express();
+
+app.use(bodyParser.json({limit:"30mb", extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
+app.use(cors());
+
+app.use('/posts', router);
+
+const CONNECTION_URL = "mongodb+srv://chandana1728:chandana1728@chandana.ulcxiue.mongodb.net/";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Running on ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+//mongoose.set('useFindAndModify', false);
